3ea7d5f6345b0d1b0dfac0fe4a6bfdfd6b24de05
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2015-09-16 10:45:46 -0500

    Upgrade to rails 4.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PPCBoard20::Application.config.session_store :cookie_store, key: '_ppc_board20_session'
+Rails.application.config.session_store :cookie_store, key: '_ppc_board20_session'
