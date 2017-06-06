321048e21407df6e3d58f09a5f64c29f63998ac0
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2017-06-05 20:17:34 -0700

    Disable spammy logs from recursive partials & cache reads in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  # Disable spammy logs from recursive partials & cache reads
+  config.action_view.logger = nil
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
