bffab3f1646ea8ceb2d3da1c7da820a559c2415c
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2017-06-05 20:54:14 -0700

    This time for sure

--- config/initializers/disable_cache_logging.rb
+++ config/initializers/disable_cache_logging.rb
@@ -1,7 +1,8 @@
 # Get rid of cache logging
 module ActionController
   class LogSubscriber < ActiveSupport::LogSubscriber
-    %w(write_fragment read_fragment exist_fragment?).each do |method|
+    %w(write_fragment read_fragment exist_fragment?
+       expire_fragment expire_page write_page).each do |method|
       class_eval <<-METHOD, __FILE__, __LINE__ + 1
         def #{method}(event)
           return
