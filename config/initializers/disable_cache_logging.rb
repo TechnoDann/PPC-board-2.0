b21ac986c8afaf6e719b334fadf7bccb9050005d
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2017-06-05 20:46:21 -0700

    Revert disabling of post subject cache, but suppress logs to its access

---
+++ config/initializers/disable_cache_logging.rb
@@ -0,0 +1,12 @@
+# Get rid of cache logging
+module ActionController
+  class LogSubscriber < ActiveSupport::LogSubscriber
+    %w(write_fragment read_fragment exist_fragment?).each do |method|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{method}(event)
+          return
+        end
+      METHOD
+    end
+  end
+end
