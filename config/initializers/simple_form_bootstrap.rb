a6080dc73f0c673247ca0e535109b9df2c6f30aa
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2019-07-04 15:20:23 -0700

    Fix the tag checkbox issue that's been a problem for forever by introducing a new wrapper style

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -63,6 +63,17 @@ SimpleForm.setup do |config|
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
+  config.wrappers :tag_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.optional :readonly
+    b.wrapper tag: 'div', class: 'col-sm-8 col-sm-offset-2' do |ba|
+      ba.use :label, class: 'control-label'
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
   config.wrappers :horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
