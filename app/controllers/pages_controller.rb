33121305c0b72016027b10804fd0a374bd23ea28
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2019-12-05 20:45:47 -0800

    Move bans to whitelisting the pages you aren't blocked from.
    Additionaly, when encountering a banned user, no longer sign them out
    at ban time.
    
    The ban whitelist has the side effect of preventing banned users from
    interacting with the Devise controllers, preventing them from creating
    new accounts and logging out via the UI.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_action :ip_ban, :check_ban, :only => [:data_collection]
   def formatting_help
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def data_collection
     respond_to do |format|
       format.html
     end
